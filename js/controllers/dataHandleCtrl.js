78a35f5e89b6523082d38ca51e6b58b026238900
Jozadaque Batista <jozadaquebatista@gmail.com>
2016-04-12 14:19:43 -0300

    updating project folders

---
+++ js/controllers/dataHandleCtrl.js
@@ -0,0 +1,7 @@
+app.controller('dataHandleCtrl', $scope=>{
+
+    $scope.userData = [{name: 'name', 'orders': 1},
+                       {name: 'name', 'orders': 1},
+                       {name: 'name', 'orders': 1},
+                       {name: 'name', 'orders': 1}];
+});
